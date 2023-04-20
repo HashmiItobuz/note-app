90fa599935a44b07ce905771648d05bb17aa7db8
Md Hashmi <md.hashmi@itobuz.com>
2023-04-20 15:59:19 -0700

    adding bars and some modification

--- script/script.js
+++ script/script.js
@@ -1,54 +1,5 @@
-console.log("Hello");
-
-// const notesPreview = document.getElementsByClassName("notes-preview")[0];
-
-// const inputTitle = document.getElementById("input-title");
-// const inputContent = document.getElementById("input-content");
-// const submit = document.getElementById("btn");
-
-// let noteArray = [];
-// submit.addEventListener("submit", function (e) {
-//   e.preventDefault();
-
-//   // let date = new Date().toLocaleDateString();
-//     let date = new Date();
-
-//     let day = date.getDate();
-//     let month = date.getMonth() + 1;
-//     let year = date.getFullYear();
-
-//     let currentDate =`${day}-${month}-${year}`;
-
-//   const content = {
-//     title: inputTitle.value,
-//     content: inputContent.value,
-//     todaysDate: currentDate
-//   }
-
-//   noteArray.push(content);
-
-//   showNotes();
-// });
-
-// function showNotes() {
-//   notesPreview.innerHTML = "";
-
-//   for (let i = 0; i < noteArray.length; i++) {
-//     let divElement = document.createElement("div");
-//     let titleText = noteArray[i].title;
-//     let contentText = noteArray[i].content;
-//     let dateText =noteArray[i].todaysDate;
-//     divElement.innerHTML = `<h3>${dateText}</h3><h1>${titleText.toUpperCase()}</h1><hr><p>${contentText}</p>`;
-//     notesPreview.appendChild(divElement);
-//     divElement.classList.add("box");
-//     inputTitle.value = "";
-//     inputContent.value = "";
-//   }
-// }
-
 const addBtn = document.getElementById("addIcon");
 const mainPage = document.querySelectorAll(".main-container");
-// const addPage = document.querySelectorAll('.container');
 const backBtn = document.getElementById("backIcon");
 
 addBtn.addEventListener("click", function () {
@@ -83,7 +34,7 @@ submit.addEventListener("click", function (e) {
   let month = date.getMonth() + 1;
   let year = date.getFullYear();
 
-  let currentDate = `${day}-${month}-${year}`;
+  let currentDate = `${day}/${month}/${year}`;
 
   const content = {
     title: inputTitle.value,
@@ -103,7 +54,7 @@ submit.addEventListener("click", function (e) {
       let titleText = noteArray[i].title;
       let descriptionText = noteArray[i].description;
       let dateText = noteArray[i].todaysDate;
-      divElement.innerHTML = `<h3>${dateText}</h3><h1>${titleText.trim()}</h1><hr/><p>${descriptionText.trim()}</p>`;
+      divElement.innerHTML = `<h5>${dateText}</h5><h4>${titleText.trim()}</h4><p>${descriptionText.trim()}</p>`;
       notesPreview.appendChild(divElement);
       divElement.classList.add("box");
       inputTitle.value = "";
